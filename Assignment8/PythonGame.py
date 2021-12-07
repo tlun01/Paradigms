e76ba20108aa880ea5f58dd14b3031e0fcaee343
tlun01 <tmlunsford01@gmail.com>
2021-12-07 16:31:13 -0600

    reeee

--- Assignment8/PythonGame.py
+++ Assignment8/PythonGame.py
@@ -5,27 +5,148 @@ from pygame.locals import*
 from time import sleep
 
 class Sprite():
-	def __init__(self):
+	def __init__(self, x1, y1, w1, h1, image_url):
+		self.x = x1
+		self.y = y1
+		self.w = w1
+		self.h = h1
+		self.flip = False
+		# self.image = 
+		# self.image.src = image_url
+	
+	def checkCollsion(self, s):
+		if(self.x + self.w <= s.x):  #this right < sprite's left
+			return False
+		if(self.x >= s.x + s.w):  #this left > sprite's right
+			return False
+		if(self.y >= s.y + s.h):  #this top underneath sprite base
+			return False
+		if(self.y + self.h <= s.y):   #this base over sprite's top
+			return False
+		return True
+
+
+class Brick(Sprite):
+	def __init__(self, x, y, w, h, image_url):
+		Sprite.__init__(x, y, w, h, image_url)
+
+	def update(self):
+		return True
+
+
+class Mario(Sprite):
+	def __init__(self, x, y, w, h, image_url):
+		Sprite.__init__(x, y, w, h, image_url)
+		self.flip = False
+		self.vert_vel = 0.0
+
+		self.screenLocation = 150
+		self.imageNum = 0
+
+		self.py = 0
+		self.px = 0
+
+		self.marioImage1 = Image()
+		self.marioImage1.src = "mario1.png"
+		self.marioImage2 = Image()
+		self.marioImage2.src = "mario2.png"
+		self.marioImage3 = Image()
+		self.marioImage3.src = "mario3.png"
+		self.marioImage4 = Image()
+		self.marioImage4.src = "mario4.png"
+		self.marioImage5 = Image()
+		self.marioImage5.src = "mario5.png"
+
+		self.marioImages = []
+		self.marioImages.append(self.marioImage1)
+		self.marioImages.append(self.marioImage2)
+		self.marioImages.append(self.marioImage3)
+		self.marioImages.append(self.marioImage4)
+		self.marioImages.append(self.marioImage5)
+
+	def update(self):
+		if(self.vert_vel != 0.0): #if not standing on ground or brick start counting frames
+			self.frameCounter += 1
+		else:   #once landed set frames back to zero
+			self.frameCounter = 0
+
+		self.savePrevY()
+		self.vert_vel += 1.2
+		self.y += self.vert_vel
+
+		if(self.y > 350):
+			self.vert_vel = 0.0
+			self.y = 350; # snap back to the ground
+
+		return True
+
+	def getOutOfTheObstacle(self, s):
+		if(self.x + self.w > s.x and self.px + self.w <= s.x): #collision with left side of brick
+				self.x = s.x - self.w
+		if(self.x < s.x + s.w and self.px >= s.x + s.w): #collision with right side of brick     
+				self.x = s.x + s.w
+		if(self.y + self.h >= s.y and self.py + self.h <= s.y):   #collision with top of brick
+			self.y = s.y - self.h
+			self.vert_vel = 0.0
+		if(self.y <= s.y + s.h and self.py >= s.y + s.h): #collision with bottom of brick
+			self.y = s.y + s.h + 1
+			self.frameCounter = 6
+			self.vert_vel = 0.1
+			if(s.isCoinBrick()):
+				return True
+			return False
+	
+	def savePrevX(self):
+		self.px = self.x
+	
+	def savePrevY(self):
+		self.py = self.y
+
+	def updateImageNum(self):
+		self.imageNum += 1
+		if(self.imageNum > 4):
+			self.imageNum = 0
 		
+		self.image = self.marioImages[self.imageNum]
+
 
 class Model():
 	def __init__(self):
-		self.dest_x = 0
-		self.dest_y = 0
+		self.sprites = []
+		self.mario = Mario(0, 350, 60, 95, "mario1.png")
+		self.sprites.append(self.mario)
+		self.brick = Brick(400, 100, 50, 50, "brick.png")
+		self.sprites.append(self.brick)
+		self.brick = Brick(600, 225, 50, 50, "brick.png")
+		self.sprites.append(self.brick)
+		self.brick2 = Brick(200, 200, 50, 50, "brick.png")
+		self.sprites.append(self.brick2)
+		self.brick3 = Brick(250, 250, 50, 50, "brick.png")
+		self.sprites.append(self.brick3)
+		self.brick4 = Brick(400, 400, 50, 50, "brick.png")
+		self.sprites.append(self.brick4)
+		# self.coinBrick = CoinBrick(25, 250, 50, 50, "coinBrick.png")
+		# self.sprites.append(self.coinBrick)
+		# self.coinBrick1 = CoinBrick(100, 100, 50, 50, "coinBrick.png")
+		# self.sprites.append(self.coinBrick1)
 
 	def update(self):
-		if self.rect.left < self.dest_x:
-			self.rect.left += 10
-		if self.rect.left > self.dest_x:
-			self.rect.left -= 10
-		if self.rect.top < self.dest_y:
-			self.rect.top += 10
-		if self.rect.top > self.dest_y:
-			self.rect.top -= 10
+		for i in range(len(self.sprites)):
+			if(not self.sprites[i].update()):
+				self.sprites.splice(i,1)
+				# break
+			if(self.sprites[i].isBrick() or self.sprites[i].isCoinBrick()):
+				if(self.mario.checkCollision(self.sprites[i])):
+					self.mario.getOutOfTheObstacle(self.sprites[i])
+						# turn ^ into an if statement after adding coins and coinbricks
+						# self.coin = Coin(self.sprites[i].x, self.sprites[i].y, self.sprites[i].w, self.sprites[i].h, "coin.png")
+						# self.sprites.push(self.coin)
+						# self.sprites[i].coinCounter += 1
+						# if(self.sprites[i].coinCounter >= 5):
+						# 	self.tempBrick = Brick(self.sprites[i].x, self.sprites[i].y, self.sprites[i].w, self.sprites[i].h, "brick.png")
+						# 	self.sprites.push(self.tempBrick)
+						# 	self.sprites.splice(i, 1)
 
-	def set_dest(self, pos):
-		self.dest_x = pos[0]
-		self.dest_y = pos[1]
 
 class View():
 	def __init__(self, model):
@@ -41,8 +162,9 @@ class View():
 		pygame.display.flip()
 
 class Controller():
-	def __init__(self, model):
+	def __init__(self, model, view):
 		self.model = model
+		self.view = view
 		self.keep_going = True
 
 	def update(self):
@@ -52,17 +174,22 @@ class Controller():
 			elif event.type == KEYDOWN:
 				if event.key == K_ESCAPE:
 					self.keep_going = False
-			elif event.type == pygame.MOUSEBUTTONUP:
-				self.model.set_dest(pygame.mouse.get_pos())
+					
 		keys = pygame.key.get_pressed()
 		if keys[K_LEFT]:
-			self.model.dest_x -= 10
+			self.model.mario.flip = True
+			self.model.mario.x -= 5
+			self.model.mario.updateImageNum()
+			self.view.backgroundLocation += 2
 		if keys[K_RIGHT]:
-			self.model.dest_x += 10
+			self.model.mario.flip = False
+			self.model.mario.x += 5
+			self.view.backgroundLocation -= 2
+			self.model.mario.updateImageNum()
 		if keys[K_UP]:
-			self.model.dest_y -= 10
-		if keys[K_DOWN]:
-			self.model.dest_y += 10
+			if(self.model.mario.frameCounter < 5):
+				self.model.mario.vert_vel -= 5.3
+
 print("Use the arrow keys to move. Press Esc to quit.")
 pygame.init()
 m = Model()
